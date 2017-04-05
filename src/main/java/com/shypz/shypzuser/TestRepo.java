9331170b4b0e6408fa8dc27ea89966589f2dc1c2
Logistics-Shypz <aanubhav.nanda@gmail.com>
2017-04-05 18:55:46 +0530

    Third Commit

--- src/main/java/com/shypz/shypzuser/TestRepo.java
+++ src/main/java/com/shypz/shypzuser/TestRepo.java
@@ -1,10 +1,11 @@
+package com.shypz.shypzuser;
 
-public class Test {
+public class TestRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing repo changes");
+		System.out.println("Testing Repo");
 
 	}
 

2152dcf3ec18a480cc4273cad7a931d49512046f
Logistics-Shypz <aanubhav.nanda@gmail.com>
2017-04-05 19:54:21 +0530

    Adding Spring Boot Class

---
+++ src/main/java/com/shypz/shypzuser/ShypzUserApiApp.java
@@ -0,0 +1,16 @@
+package com.shypz.shypzuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShypzUserApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(ShypzUserApiApp.class,args );
+
+	}
+
+}

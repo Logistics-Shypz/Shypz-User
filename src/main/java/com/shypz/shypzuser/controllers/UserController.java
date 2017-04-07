aaae87126b370377914a9309c9f192646154be79
Logistics-Shypz <aanubhav.nanda@gmail.com>
2017-04-07 12:27:57 +0530

    Adding method in UserService to get User by Id

--- src/main/java/com/shypz/shypzuser/controllers/UserController.java
+++ src/main/java/com/shypz/shypzuser/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.shypz.shypzuser.controllers;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,12 @@ public class UserController {
 		
 		
 		return userService.getAllUsers();
+	}
+	
+	@RequestMapping("/users/{id}")
+	public User getUserById(@PathVariable int id){
+		
+		return userService.getUserById(id);
 	}
 
 }
--- src/main/java/com/shypz/shypzuser/services/UserService.java
+++ src/main/java/com/shypz/shypzuser/services/UserService.java
@@ -2,6 +2,7 @@ package com.shypz.shypzuser.services;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.springframework.stereotype.Service;
 
@@ -14,13 +15,27 @@ public class UserService {
 	private List<User> users = Arrays.asList(
 			new User(1,"anubhav","anubhav.nanda@gmail.com","dgdfgdfgdf","546345346"),
 			new User(2,"mukesh","mukesh14j@gmail.com","dghghgrethgre2","4564554654"),
-			new User()
+			new User(3,"test","test@gmail.com","dgdgdggdfd","43534534534")
 		);
 	
 	
 	public List<User> getAllUsers(){
 		
 		return users;
+	}
+	
+	public User getUserById(int id){
+		User uret = new User();
+		for(User u:users){
+			
+			if(u.getUser_Id() == id){
+				System.out.println("Matched");
+				uret = u;
+			}
+		}
+		
+		return uret;
+		
 	}
 	
 	
